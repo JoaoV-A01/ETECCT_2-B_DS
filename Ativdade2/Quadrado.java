906bea52982156909811167bd7fba6d43b5ab698
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2021-03-19 18:38:15 -0300

    Add files via upload

---
+++ Ativdade2/Quadrado.java
@@ -0,0 +1,14 @@
+//Classe
+public class Quadrado {
+	double base;
+	double altura;
+	double area;
+	//Função com retorno mas sem parametro
+		double calcularArea (){
+	//Calculo 
+		area = base * altura;
+		
+		return area;
+		
+	}
+}
