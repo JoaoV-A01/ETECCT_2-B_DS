a2bf69a712a67079a962a02fca5844f2c634117b
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2021-07-31 21:11:58 -0300

    Create Funcionario.java

---
+++ Herança/Funcionario.java
@@ -0,0 +1,32 @@
+//Classe Funcionario
+public class Funcionario {
+	private String nome;
+	private String cpf;
+	protected int salaro;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.nome = nome;
+	}
+
+	public String getSalario() {
+		return nome;
+	}
+	public void setSalario(int salario) {
+		this.nome = nome;
+	}
+
+	pubic double getBonofocacao() {
+		return salaro * 1.1;
+	}
+
+}
