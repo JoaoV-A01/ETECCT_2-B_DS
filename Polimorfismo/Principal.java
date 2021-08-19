455d20cdb396bd26e33de7f16539367cb5238ff4
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2021-08-19 17:05:39 -0300

    Create Principal.java

---
+++ Polimorfismo/Principal.java
@@ -0,0 +1,17 @@
+//Classe Principal
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		cachorro c = new cachorro();
+		c.caracteristicas();
+		
+		elefante e = new elefante();
+		e.caracteristicas();
+		
+		jaguar j = new jaguar();
+		j.caracteristicas();
+			
+	}
+}
