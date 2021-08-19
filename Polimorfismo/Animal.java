2fe136c6d0e62f1da478db75472df2b26580da37
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2021-08-19 17:03:25 -0300

    Create Animal.java

---
+++ Polimorfismo/Animal.java
@@ -0,0 +1,23 @@
+//Classe animal
+public class animal {
+	public String cor;
+	public double tamanho;
+	public String nome;
+	
+	public int correr(int km) {
+		return km;
+	}
+	public String alimentar(String alimento) {
+		return alimento;
+	}
+	public int dormir(int tempo) {
+		return tempo;
+	}
+	public String emitirSom(String som) {
+		return som;
+	}
+	
+	public void caracteristicas() {
+
+	}
+}
