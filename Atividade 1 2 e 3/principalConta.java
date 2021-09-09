ecf9aeda40d6bee7f30026ede908563b95e54f73
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2021-09-09 15:21:35 -0300

    Create principalConta.java

---
+++ Atividade 1 2 e 3/principalConta.java
@@ -0,0 +1,15 @@
+public class principalConta {
+
+	public static void main(String[] args) {
+		Conta primeiro = new Conta();
+		primeiro.saldo = 200;
+
+		System.out.println("Saldo na conta inicial" +" = "  +primeiro.saldo);
+		primeiro.depositar(200);
+		System.out.println("Saldo após o deposito" +" = " +primeiro.extrato());
+		System.out.println(primeiro.sacar(500));
+		System.out.println("Saldo final da conta" +" = " +primeiro.saldo);
+		System.out.println(primeiro.extrato());
+	}
+
+}
